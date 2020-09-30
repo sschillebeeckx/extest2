0f9d6dfcde2d7a317e995e51443dfa0bb92873de
Sandy <sschillebeeckx@abis.be>
2020-09-30 11:49:58 +0200

    ExA2

---
+++ ExA2/src/test/java/be/abis/exa2/ut/TestPerson.java
@@ -0,0 +1,16 @@
+package be.abis.exa2.ut;
+
+import be.abis.exa2.model.Person;
+import org.junit.Test;
+import java.time.LocalDate;
+import static org.junit.Assert.assertEquals;
+
+public class TestPerson {
+
+	@Test
+	public void testCalculateAge() {
+		Person p2 = new Person(2,"John","Doe",LocalDate.of(1967, 8, 10));
+		assertEquals(p2.calculateAge(),53);
+	}
+
+}
