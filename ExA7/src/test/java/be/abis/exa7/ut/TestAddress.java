24f6773b72fef256ff363fcf9b7a529eacaf6a7a
Sandy <sschillebeeckx@abis.be>
2020-09-30 16:48:01 +0200

    added A7 and A8

---
+++ ExA7/src/test/java/be/abis/exa7/ut/TestAddress.java
@@ -0,0 +1,22 @@
+package be.abis.exa7.ut;
+
+
+import be.abis.exa7.model.Address;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestAddress {
+
+    @Test
+    public void testThatBelgianZipCodeIsNumeric() {
+        //arrange
+        Address a = new Address("Diestsevest","32 bus 4B","3000","Leuven","Belgium","BE");
+
+        //act
+        boolean b = a.checkBelgianZipCode();
+
+        //assert
+        assertTrue(b);
+    }
+}
